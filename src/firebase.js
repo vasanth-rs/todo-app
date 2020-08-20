80e90be46be0f91cc5911ae3d6b92d39d7566f66
vasanth-rs <69900991+vasanth-rs@users.noreply.github.com>
2020-08-20 08:48:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyClb1cOV1KVkwSH6te9ylPgMXB2r3XcEB0",
-  authDomain: "todo-app-91643.firebaseapp.com",
-  databaseURL: "https://todo-app-91643.firebaseio.com",
-  projectId: "todo-app-91643",
-  storageBucket: "todo-app-91643.appspot.com",
-  messagingSenderId: "108128551755",
-  appId: "1:108128551755:web:ec694c17cae2e73883dece",
-  measurementId: "G-YFB4D1DXE4",
+  //your firebase sdk config
+   apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 });
 
 const db = firebaseApp.firestore();
